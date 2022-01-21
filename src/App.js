fabe6eee03eb170207bdc2ec07b4d4b8f103353d
Fahad Hussain <sfahadh14@gmail.com>
2022-01-21 14:47:01 -0500

    created xy grid

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import './App.css';
 
 function App() {
 	return (
-		<div>
-
-		</div>
+		<section className='graph'>
+			<div className='top-left'></div>
+			<div className='top-right'></div>
+			<div className='bottom-left'></div>
+			<div className='bottom-right'></div>
+		</section>
 	);
 }
 
